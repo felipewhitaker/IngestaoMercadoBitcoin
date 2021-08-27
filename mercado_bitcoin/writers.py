b898c3a0fb5a5d3bf0f11ac8c64f92f3feb5ed18
Felipe Whitaker <felipewhitaker@gmail.com>
2021-08-26 22:03:29 -0300

    Change s3 bucket name

--- mercado_bitcoin/writers.py
+++ mercado_bitcoin/writers.py
@@ -2,9 +2,11 @@ import datetime
 import json
 import os
 from tempfile import NamedTemporaryFile
-from typing import List
+from typing import List, Union
 
 import boto3
+
+BUCKET = 'thor-datalake-mercado'
 
 
 class DataTypeNotSupportedForIngestionException(Exception):
@@ -25,7 +27,7 @@ class DataWriter:
         with open(self.filename, "a") as f:
             f.write(row)
 
-    def _write_to_file(self, data: [List, dict]):
+    def _write_to_file(self, data: Union[List, dict]):
         if isinstance(data, dict):
             self._write_row(json.dumps(data) + "\n")
         elif isinstance(data, List):
@@ -34,7 +36,7 @@ class DataWriter:
         else:
             raise DataTypeNotSupportedForIngestionException(data)
 
-    def write(self, data: [List, dict]):
+    def write(self, data: Union[List, dict]):
         self._write_to_file(data=data)
 
 
@@ -49,11 +51,12 @@ class S3Writter(DataWriter):
         with open(self.tempfile.name, "a") as f:
             f.write(row)
 
-    def write(self, data: [List, dict]):
+    def write(self, data: Union[List, dict]):
         self._write_to_file(data=data)
         self._write_file_to_s3()
 
     def _write_file_to_s3(self):
+        global BUCKET
         self.client.put_object(
-            Body=self.tempfile, Bucket="belisco-data-lake-raw", Key=self.key
+            Body=self.tempfile, Bucket=BUCKET, Key=self.key
         )
